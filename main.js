f72ea9ce90324e53bf41ebdd56a6376ea3373eb7
joaobonelli6 <joao.koch@escola.pr.gov.br>
2026-04-29 08:12:53 -0300

    alteração de data

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-04-25T23:00:00");
-const tempoObjetivo2 = new Date("2026-04-25T23:00:00");
-const tempoObjetivo3 = new Date("2026-04-25T23:00:00");
-const tempoObjetivo4 = new Date("2026-04-25T23:00:00");
+const tempoObjetivo1 = new Date("2027-01-01T23:00:00");
+const tempoObjetivo2 = new Date("2026-12-25T23:00:00");
+const tempoObjetivo3 = new Date("2026-10-10T23:00:00");
+const tempoObjetivo4 = new Date("2026-12-01T23:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
